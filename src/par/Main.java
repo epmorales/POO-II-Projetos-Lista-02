60ee31a3df3cae29a8231503e14bb4f388a88976
Eric Morales <ericmorales2009@hotmail.comm>
2025-09-07 00:40:02 -0300

    atividade par

--- src/par/Main.java
+++ src/par/Main.java
@@ -1,4 +1,12 @@
 package par;
 
 public class Main {
+    public static void main(String[] args) {
+        Par<String, Integer> nome = new Par<>("Eric", 43);
+        Par<Integer, String> sala = new Par<>(306, "Laboratório de Informática");
+
+        System.out.println("Nome: " + nome.getChave() + " - " + nome.getValor() + " anos");
+        System.out.println("Sala: " + sala.getChave() + " - " + sala.getValor());
+    }
 }
+
